4b0344c959b8a2d89ffea82af5e395c8bf59426b
asim <asimshrestha608@gmail.com>
2024-07-29 18:05:03 +0545

    create new route

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <div>Chai Page</div>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2 hover:bg-blue-700">Button</button>
+    </main>
+  );
+}
