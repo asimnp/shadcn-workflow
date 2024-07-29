4db424fdec8da2a9666cdef6a64590fd81a5683f
asim <asimshrestha608@gmail.com>
2024-07-29 18:19:10 +0545

    use shadcn button

--- src/app/chai/page.tsx
+++ src/app/chai/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
       <div>Chai Page</div>
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2 hover:bg-blue-700">Button</button>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-lg my-4 hover:bg-blue-700">
+        Button
+      </button>
+      <Button>Shadcn</Button>
     </main>
   );
 }
