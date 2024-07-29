d8137a3fb5dabe2278dd4bdaca2af2f22a26c358
asim <asimshrestha608@gmail.com>
2024-07-29 18:54:57 +0545

    use accordion with custom content

--- src/app/accordin/page.tsx
+++ src/app/accordin/page.tsx
@@ -1,7 +1,31 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
+  const faq = [
+    {
+      question: "What is good?",
+      answer: "This is good",
+    },
+    {
+      question: "What is neutral?",
+      answer: "This is neutral",
+    },
+  ];
   return (
-    <main>
-      <div>Accordin</div>
+    <main className="flex justify-center items-center w-[500px]">
+      <Accordion type="single" collapsible>
+        {faq.map((f) => (
+          <AccordionItem value={f.question} key={f.question}>
+            <AccordionTrigger>{f.question}</AccordionTrigger>
+            <AccordionContent>{f.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </main>
   );
 }
